dfb08f5863b86691046fbf97581283233871927a
ragnar-codes <yessine.agrebi.aiesec@gmail.com>
2022-04-25 00:28:08 +0200

    get users

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 import express from 'express';
 
 const app = express();
-const PORT = 3000;
+const port = process.env.PORT || 3000;
+
+const courses = [
+    {id: 1, name: "JavaScript"},
+    {id: 2, name: "Java"},
+    {id: 3, name: "PHP"},
+]
 
 app.get('/', (req, res) => {
     res.send('Demo App');
 });
 
+app.get('/api/courses', (req, res) => {
+    res.send(courses);
+})
 
-app.listen(3000, () => console.log(`server running on the port ${PORT}`));
+app.get('/api/courses/:id', (req, res) => {
+   let course = courses.find(course => course.id === parseInt(req.params.id));
+   if(!course) res.status(404).send('The course with the given id not found')
+   res.send(course);
+})
+
+
+app.listen(port, () => console.log(`server running on the port ${port}`));
