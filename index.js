b4cafed33d46624ee412f561f3d7515f7fd99b25
ragnar-codes <yessine.agrebi.aiesec@gmail.com>
2022-04-25 02:24:36 +0200

    handling delete requests and fix some bugs

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const courses = [
     {id: 1, name: "JavaScript"},
     {id: 2, name: "Java"},
     {id: 3, name: "PHP"},
+    {id: 4, name: "laravel"},
 ]
 
 app.get('/', (req, res) => {
@@ -21,17 +22,15 @@ app.get('/api/courses', (req, res) => {
 app.get('/api/courses/:id', (req, res) => {
    let course = courses.find(course => course.id === parseInt(req.params.id));
    // validate user input
-   if(!course) res.status(404).send('The course with the given id not found')
+   if(!course) return res.status(404).send('The course with the given id not found')
    res.send(course);
 });
 
 app.post('/api/courses', (req, res) => {
     const {error} = validateCourse(req.body);
-    if(error){
-        // bad request 400
-        res.status(400).send(error.details[0].message);
-        return; // we don't want the rest of the function to be executed
-    }
+    if(error) return res.status(400).send(error.details[0].message);
+ 
+
     const course = {
         id: courses.length + 1,
         name: req.body.name
@@ -44,7 +43,9 @@ app.put('/api/courses/:id', (req, res) => {
     //look up the course
     //if not existing, return 404
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course) res.status(404).send('The course with the given id not found')
+    if(!course) return res.status(404).send('The course with the given id not found');
+
+    
     const {error} = validateCourse(req.body); // result.error
     
     if(error){
@@ -57,6 +58,16 @@ app.put('/api/courses/:id', (req, res) => {
     //return the updated course
     res.send(course);
     
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) return res.status(404).send('The course with the given id not found');
+        
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1)// remove 1 object from the given index
+    res.send(course);
 });
 
 const validateCourse = (course) => {
